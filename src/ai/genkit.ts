8766b83e7011c6b2eb6e782489b00f24626c7ba6
Nehemie Destine <cyberdestine@gmail.com>
2025-07-18 22:16:04 +0000

    connect to vertex ai https://console.cloud.google.com/vertex-ai/studio/s

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,13 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      vertex: {
+        project: 'kasik-alpha-trade',
+        location: 'us-central1',
+      },
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
