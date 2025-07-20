7fd5ef13b6e2d6d9d4ed3416e92013a0354b1476
Nehemie Destine <cyberdestine@gmail.com>
2025-07-20 04:12:42 +0000

    # 1. Fix your `dev` script once and for all Open **package.json** in y

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  experimental: {
+    allowedNextDevOrigins: [
+      "https://6000-firebase-studio-*.cloudworkstations.dev",
+      "https://9003-firebase-studio-*.cloudworkstations.dev"
+    ]
+  }
 };
 
 module.exports = nextConfig;
